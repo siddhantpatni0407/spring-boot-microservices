3bff8c75c6779f271e193275d67e947b1413d61b
Siddhant Patni <45511204+siddhantpatni0407@users.noreply.github.com>
2024-03-04 14:00:41 +0530

    Added SQL Script
    Added SQL Script

---
+++ spring-boot-batch-partitioning-service/src/main/resources/sql_script.sql
@@ -0,0 +1,7 @@
+select * from dev.customers_info;
+select * from dev.batch_job_execution;
+select * from dev.batch_job_execution_context;
+select * from dev.batch_job_execution_params;
+select * from dev.batch_job_instance;
+select * from dev.batch_step_execution;
+select * from dev.batch_step_execution_context;
